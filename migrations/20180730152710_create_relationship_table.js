0d85b683bc2543155c32e143cf2c528e90992844
voitovych <chumba.vumbaa@gmail.com>
2018-07-30 15:41:32 +0300

    Created one-side-relationship table

--- migrations/20180730152710_create_relationship_table.js
+++ migrations/20180730152710_create_relationship_table.js
@@ -1,8 +1,15 @@
 
 exports.up = (knex, Promise) => {
-  
+  return knex.schema.createTable("relationship", (table) => {
+    table.increments("relationship_id"); // id serial primary key
+    table.integer("account_id_who_share_data").unsigned();
+    table.integer("account_id_who_receive_data").unsigned();
+
+    table.foreign("account_id_who_share_data").references("account_id").inTable("account");
+    table.foreign("account_id_who_receive_data").references("account_id").inTable("account");
+  });
 };
 
 exports.down = (knex, Promise) => {
-  
+  return knex.schema.dropTable("relationship");
 };
